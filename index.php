2a6f7be0c17c5d18e63f7fc3cc884b1ddb5c6166
ClementElectron <clement.f@tuta.io>
2017-08-11 00:12:59 +0200

    Applied changes on span tags for obfuscation

--- index.php
+++ index.php
@@ -41,13 +41,12 @@ if(empty($_GET['key']) || htmlspecialchars($_GET['key']) != '3s2o5sALyNORreaZK4w
             <article>
                 <h2>Contact Me</h2>
                 <ul class="contact-list">
-                    <li><span class="icon-email"></span> <?=contactLink('mailto:foo@bar.com', 'Contact me by email.', 'foo@bar.com')?></li>
-                    <li><span class="icon-linkedin"></span> <?=contactLink('https://www.linkedin.com/in/...', 'LinkedIn', '...')?></li>
-                    <li><span class="icon-twitter"></span> <?=contactLink('https://twitter.com/...', 'Follow me on Twitter.', '@...')?></li>
-                    <li><span class="icon-500px"></span> <?=contactLink('https://500px.com/...', 'Visit my 500px gallery.', '500px.com/...')?></li>
-                    <li><span class="icon-github"></span> <?=contactLink('https://github.com/...', 'Visit my GitHub repository.', 'GitHub.com/...')?></li>
-                    <li><span class="icon-keybase"></span> <?=contactLink('https://keybase.io/...', 'My KeyBase.io', 'KeyBase.io/...')?></li>
-                    <li><span class="icon-signal"></span> <span title="Call me with Signal."><?=encodeToHtmlEntities('+...')?></span></li><!-- Signal must stay at end of list. -->
+                    <li><span class="icoeml"></span> <?=contactLink('mailto:foo@bar.com', 'Contact me by email.', 'foo@bar.com')?></li>
+                    <li><span class="icolnk"></span> <?=contactLink('https://www.linkedin.com/in/...', 'LinkedIn', '...')?></li>
+                    <li><span class="icotwi"></span> <?=contactLink('https://twitter.com/...', 'Follow me on Twitter.', '@...')?></li>
+                    <li><span class="ico5px"></span> <?=contactLink('https://500px.com/...', 'Visit my 500px gallery.', '500px.com/...')?></li>
+                    <li><span class="icogit"></span> <?=contactLink('https://github.com/...', 'Visit my GitHub repository.', 'GitHub.com/...')?></li>
+                    <li><span class="icokey"></span> <?=contactLink('https://keybase.io/...', 'My KeyBase.io', 'KeyBase.io/...')?></li>
                 </ul>
                 <p>If you want to, <a href="..." class="hvr-shutter-out-vertical" title="Click to download my PGP public key." target="_blank">download my PGP public key</a>, here you have the fingerprint:</p>
                 <p class="center"><code id="fingerprint">...</code></p>
@@ -55,7 +54,7 @@ if(empty($_GET['key']) || htmlspecialchars($_GET['key']) != '3s2o5sALyNORreaZK4w
             <noscript>
                 <article>
                     <h2>Privacy Policy</h2>
-                    <p class="justify">If you enable JavaScript, your IP address and the date time will be saved in a database I own. I don't mind who you are and I respect your privacy, I don't track you. I just like to know how many visitors are reading this website day after day.</p>
+                    <p class="justify">If you enable JavaScript, your IP address and the date time will be saved in a database I own. That is not the case since you disabled JS. My purpose is to know how many visitors are reading my web page day after day. In other words, the log is for statistics rather than tracking.</p>
                 </article>
             </noscript>
         </section>
@@ -64,11 +63,12 @@ if(empty($_GET['key']) || htmlspecialchars($_GET['key']) != '3s2o5sALyNORreaZK4w
         </script>
         <div id="materialModal" onclick="closeMaterialAlert(event, false)" class="hide">
             <div id="materialModalCentered">
-                <div id="materialModalContent" onclick="event.stopPropagation()">
-                    <h1>Privacy Policy</h1>
-                    <p id="materialModalText" class="justify">Your IP address and the date time are saved in a database I own. I don't mind who you are and I respect your privacy, I don't track you. I just like to know how many visitors are reading this website day after day. If you don't want your IP address to be remembered in the future, just disable JavaScript, you won't have any problem reading this page without JS.</p>
-                    <div id="materialModalButtons"><button id="materialModalButtonOK" class="materialModalButton hvr-box-shadow-outset" onclick="closeMaterialAlert(event, true)">OK</button></div>
-                </div>
+			<div id="materialModalCentered">
+				<div id="materialModalContent" onclick="event.stopPropagation()">
+					<h1>Privacy Policy</h1>
+					<p id="materialModalText" class="justify">Your IP address and the date time are saved in a database I own. My purpose is to know how many visitors are reading my web page day after day. In other words, the log is for statistics rather than tracking. If you don't want your IP address to be remembered in the future, just disable JavaScript, you won't have any problem reading this page without JS.</p>
+					<div id="materialModalButtons"><button id="materialModalButtonOK" class="materialModalButton hvr-box-shadow-outset" onclick="closeMaterialAlert(event, true)">OK</button></div>
+				</div>
             </div>
         </div>
     </body>
